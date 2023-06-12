c258fcc2ca11dde98c74b5987e380d3e75e847cd
michaelayisi <mayisi581@gmail.com>
2023-06-12 08:49:52 +0300

    final

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const config = {
     // BASE_URL: 'http://localhost:8888/myserver/api/',
-    BASE_URL: ' https://ahara-parser.herokuapp.com/myserver/api/',
+    BASE_URL: 'https://ahara-json-viewer.herokuapp.com/myserver/api/',
 };
 export default config;  
